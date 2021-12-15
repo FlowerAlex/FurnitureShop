37ba63676b6d5884e38f7d143282a7476326a5be
BobAesthetic <michalklewicki@list.pl>
2021-12-15 20:21:32 +0000

    registering works

--- backend/src/Core/FurnitureShop.Core.Services/CQRS/Mobile/Orders/CreateOrderCH.cs
+++ backend/src/Core/FurnitureShop.Core.Services/CQRS/Mobile/Orders/CreateOrderCH.cs
@@ -23,16 +23,16 @@ namespace FurnitureShop.Core.Services.CQRS.Mobile.Orders
 
         public async Task ExecuteAsync(CoreContext context, CreateOrder command)
         {
-            var result = await dbContext.Orders.AddAsync(mapper.Map<Order>(command.NewOrder));
-                // new Order(command.NewOrder.Street, command.NewOrder.City, command.NewOrder.State,
-                //     command.NewOrder.PostalCode, command.NewOrder.Country)
-                // {
-                //     UserId = Id<User>.From(command.NewOrder.UserId),
-                //     Price = command.NewOrder.Price,
-                //     OrderedDate = DateTime.Now,
-                //     OrdrerState = OrderState.Pending,
-                //     Products = new List<Product>(), // command.NewOrder.Products.Select( p => new Pro) TODO
-                // });
+            var result = await dbContext.Orders.AddAsync(//mapper.Map<Order>(command.NewOrder));
+                new Order(command.NewOrder.Street, command.NewOrder.City, command.NewOrder.State,
+                    command.NewOrder.PostalCode, command.NewOrder.Country)
+                {
+                    UserId = Id<User>.From(command.NewOrder.UserId),
+                    Price = command.NewOrder.Price,
+                    OrderedDate = DateTime.Now,
+                    OrdrerState = OrderState.Pending,
+                    Products = new List<Product>(), // command.NewOrder.Products.Select( p => new Pro) TODO
+                });
         }
     }
 }
