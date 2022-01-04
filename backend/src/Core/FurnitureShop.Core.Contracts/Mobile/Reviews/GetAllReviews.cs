31f35eb6f3dc2d94b28d2d5d2a9987ee6bba49f5
Michał Klewicki <michal.klewicki@ascron.com>
2022-01-04 16:45:44 +0100

    added get all reviews

--- backend/src/Core/FurnitureShop.Core.Contracts/Mobile/Reviews/GetAllReviews.cs
+++ backend/src/Core/FurnitureShop.Core.Contracts/Mobile/Reviews/GetAllReviews.cs
@@ -5,17 +5,9 @@ using LeanCode.CQRS.Security;
 
 namespace FurnitureShop.Core.Contracts.Mobile.Reviews
 {
-    // [AuthorizeWhenHasAnyOf(Auth.Roles.User,Auth.Roles.Admin)]
-    [AllowUnauthorized]
-    public class GetAllReviews : IRemoteQuery<List<ReviewDTO>> // TODO paginated query
+    [AuthorizeWhenHasAnyOf(Auth.Roles.User,Auth.Roles.Admin)]
+    public class GetAllReviews : PaginatedQuery<ReviewDTO> 
     {
-        public Guid? ProductId { get; set; }
-        public Guid? UserId { get; set; }
-        public static class ErrorCodes
-        {
-            public const int IncorrectName = 1;
-            public const int IncorrectDescription = 2;
-            public const int IncorrectPrice = 3;
-        }
+        public Guid ProductId { get; set; }
     }
 }
