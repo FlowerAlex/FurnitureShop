edba8d37402623dcc7a1274a2171a8fb79813947
Michał Klewicki <michal.klewicki@ascron.com>
2022-01-10 18:05:05 +0100

    added shopping cart to db

---
+++ backend/src/Core/FurnitureShop.Core.Domain/ShoppingCart.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using LeanCode.DomainModels.Model;
+using LeanCode.Time;
+namespace FurnitureShop.Core.Domain
+{
+    public class ShoppingCart : IAggregateRoot<Id<ShoppingCart>>
+    {
+        public Id<ShoppingCart> Id { get; set; }
+        public Id<User>? UserId { get; set; }
+        public List<ShoppingCartProduct> ShoppingCartProducts { get; set; }
+        byte[] IOptimisticConcurrency.RowVersion { get; set; } = Array.Empty<byte>();
+        DateTime IOptimisticConcurrency.DateModified { get; set; }
+
+        public ShoppingCart()
+        {
+            ShoppingCartProducts = new List<ShoppingCartProduct>();
+        }
+    }
+}
---
+++ backend/src/Core/FurnitureShop.Core.Domain/ShoppingCartProduct.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using LeanCode.DomainModels.Model;
+using LeanCode.Time;
+
+namespace FurnitureShop.Core.Domain
+{
+    public class ShoppingCartProduct : IAggregateRoot<Id<ShoppingCartProduct>>
+    {
+        
+        public Id<ShoppingCartProduct> Id { get; set; }
+        public Id<ShoppingCart>? ShoppingCartId { get; set; }
+        public Id<Product>? ProductId { get; set; }
+        public int Amount { get; set; }
+        byte[] IOptimisticConcurrency.RowVersion { get; set; } = Array.Empty<byte>();
+        DateTime IOptimisticConcurrency.DateModified { get; set; }
+
+    }
+}
