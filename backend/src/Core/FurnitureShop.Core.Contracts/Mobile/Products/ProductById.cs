896b8e8220169362cafb75681641cd4095778086
Michal Klewicki <michal.klewicki@ascron.com>
2022-04-19 11:51:02 +0200

    products validation and auth

--- backend/src/Core/FurnitureShop.Core.Contracts/Mobile/Products/ProductById.cs
+++ backend/src/Core/FurnitureShop.Core.Contracts/Mobile/Products/ProductById.cs
@@ -4,8 +4,7 @@ using LeanCode.CQRS.Security;
 
 namespace FurnitureShop.Core.Contracts.Mobile.Products
 {
-    // [AuthorizeWhenHasAnyOf(Auth.Roles.User,Auth.Roles.Admin)]
-    [AllowUnauthorized]
+    [AuthorizeWhenHasAnyOf(Auth.Roles.User,Auth.Roles.Admin)]
     public class ProductById : IRemoteQuery<ProductWithDetailsDTO>
     {
         public Guid Id { get; set; }
