09fc9db2423aeb8a7de9ff60375461a7a38076ee
leoniklexa@gmail.com <leoniklexa@gmail.com>
2022-06-26 17:17:04 +0200

    Add allowed host to launch.json & add log out button to web

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -4,6 +4,6 @@ import 'package:furniture_shop/main_common.dart';
 void main() {
   final config = AppConfig(
       debugMode: true,
-      apiUri: Uri.parse('https://56ee-91-202-125-36.ngrok.io'));
+      apiUri: Uri.parse('https://d7f6-91-202-125-36.ngrok.io'));
   mainCommon(config);
 }
--- web/lib/main_screen/main_screen.dart
+++ web/lib/main_screen/main_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:furniture_shop/features/auth/auth_cubit.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final authCubit = context.read<AuthCubit>();
+
+    return Scaffold(
       body: Center(
-        child: Text('Hello World!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Hello World!'),
+            OutlinedButton(
+              onPressed: () async {
+                await authCubit.logOut();
+              },
+              child: const Text('Log out'),
+            ),
+          ],
+        ),
       ),
     );
   }
