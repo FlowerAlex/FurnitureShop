7d5d178e0065f052e068174dee9391f3c275d67c
Michal Klewicki <michal.klewicki@ascron.com>
2022-08-28 12:23:25 +0200

    added all reviews for user

---
+++ backend/src/Core/FurnitureShop.Core.Services/CQRS/Web/Reviews/AllReviewsForUserQH.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FurnitureShop.Core.Contracts.Mobile.Reviews;
+using FurnitureShop.Core.Contracts.Web.Reviews;
+using FurnitureShop.Core.Contracts.Shared;
+using FurnitureShop.Core.Services.DataAccess;
+
+namespace FurnitureShop.Core.Services.CQRS.Web.Reviews
+{
+    public class AllReviewsForUserQH : IQueryHandler<AllReviewsForUser, PaginatedResult<ReviewDTO>>
+    {
+        private readonly CoreDbContext dbContext;
+
+        public AllReviewsForUserQH(CoreDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<PaginatedResult<ReviewDTO>> ExecuteAsync(CoreContext context, AllReviewsForUser query)
+        {
+            return await dbContext.Reviews
+            .Where(p => p.UserId == query.UserId)
+            .Select(p => new ReviewDTO
+            {
+                UserId = p.UserId,
+                ProductId = p.ProductId,
+                Text = string.IsNullOrWhiteSpace(p.Text) ? string.Empty : p.Text,
+                Rating = p.Rating,
+                CreatedDate = p.CreatedDate,
+                Id = p.Id,
+            })
+            .ToPaginatedResultAsync(query);
+        }
+    }
+}
